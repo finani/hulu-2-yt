31b798c53664fc03096ecdcffa8b8bb3bfae73f7
Inhwan Wee <10ezenfinani@gmail.com>
2022-03-05 01:41:29 +0900

    building the thumbnail

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
+import requests from '../utils/requests';
 
-export default function Home() {
+export default function Home( { results }) {
+  console.log(results);
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
       <Nav />
-
-      {/* Results */}
-
+      <Results results={results}/>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const url = requests[genre]?.url || requests.fetchTrending.url
+
+  const request = await fetch(
+    'https://api.themoviedb.org/3/' + url
+  ).then(res => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    }
+  }
 }
