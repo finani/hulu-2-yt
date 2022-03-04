05d5a062f87f831c40a1d853a1b2077fe3a4b45a
Inhwan Wee <10ezenfinani@gmail.com>
2022-03-05 01:54:40 +0900

    apply flip move

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,13 +1,16 @@
 import Image from "next/image"
 import { ThumbUpIcon } from '@heroicons/react/outline'
+import { forwardRef } from "react";
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   const imageUrl = (BASE_URL + (result.backdrop_path || result.poster_path)) ||
                     BASE_URL + result.poster_path;
 
   return (
-    <div className='p-2 group cursor-pointer transition duration-200
+    <div
+      ref={ref}
+      className='p-2 group cursor-pointer transition duration-200
       ease-in transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout='responsive'
@@ -30,7 +33,7 @@ function Thumbnail({ result }) {
         </p>
       </div>
     </div>
-  )
-}
+  );
+})
 
 export default Thumbnail;
